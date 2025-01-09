9086ab2634f90f63957fac218155e32b02c67afc
zytomorrow <z794672847@gmail.com>
2025-01-09 09:36:45 +0000

    更新依赖

--- import_packages.sh
+++ import_packages.sh
@@ -3,4 +3,5 @@
 trap 'rm -rf "$TMPDIR"' EXIT
 TMPDIR=$(mktemp -d) || exit 1
 
-# 
+# 使用kwrt-packages
+echo "src-git openwrt_kiddin9 https://github.com/kiddin9/kwrt-packages.git" >> feeds.conf.default
